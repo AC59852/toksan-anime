32cb493ec3c1d40efaa5ee1f58af32bf2313f332
Austin Caron <austin.caron1@gmail.com>
2021-05-08 18:13:11 -0400

    added basic tablet scaling, fixed image loading and added some parallax on tablet

--- components/sao/SaoContent.vue
+++ components/sao/SaoContent.vue
@@ -1,16 +1,22 @@
 <template>
   <section>
-      <h2 class="sao__heading">{{ block.title }}</h2>
-      <img class="sao__image" v-if="block.image != null" :src="block.image" :alt="'Sword Art Online Image of ' + block.title">
-      <p class="sao__para">{{ block.para1 }}</p>
-      <p class="sao__para">{{ block.para2 }}</p>
-      <span class="sao__quote" v-if="block.quote != null">&#8212; {{ block.quote }}</span>
+      <h2 class="sao__heading" v-if="this.mobile">{{ block.title }}</h2>
+      <picture :class="'sao__image sao__image--' + block.id" v-if="block.image != null" :src="block.image">
+        <source media="(min-width: 768px)" :srcset="block.image2">
+        <img :src="block.image" alt="">
+      </picture>
+      <div class="sao__text">
+        <h2 class="sao__heading" v-if="tablet">{{ block.title }}</h2>
+        <p class="sao__para">{{ block.para1 }}</p>
+        <p class="sao__para">{{ block.para2 }}</p>
+        <span class="sao__quote" v-if="block.quote != null">&#8212; {{ block.quote }}</span>
+      </div>
     </section>
 </template>
 
 <script>
 export default {
-    props: ['block']
+    props: ['block', 'mobile', 'tablet']
 }
 </script>
 
--- pages/sao.vue
+++ pages/sao.vue
@@ -2,22 +2,27 @@
   <section class="anime sao">
     <section class="sao__hero">
       <img class="sao__logo" src="~/assets/images/sao/sao_logo.svg" alt="Sword Art Online Logo" >
-      <div class="sao__btns">
-        <a href="https://www.youtube.com/watch?v=a4KpzxD7kzI" target="_blank">Trailer</a>
-        <div class="sao__event">Link Start</div>
-      </div>
     </section>
     <section class="sao__content sao__content--0">
-      <h1 class="sao__heading sao__heading--white">The World of Swords</h1>
-      <p class="sao__para sao__para--white">In the year 2022, a next generation game device known as “NerveGear” has been developed, making Full Dives into a virtual dimension possible. “NerveGear” is the world’s first true VRMMORPG device.</p>
-      <p class="sao__para sao__para--white">"Sword Art Online (SAO)" has generated worldwide buzz, and on its official launch day, 10’000 players immerse themselves in the virtual world, only to find out that none of them can escape.</p>
+      <h1 v-if="mobile" class="sao__heading sao__heading--white">The World of Swords</h1>
+      <picture v-if="tablet" class="sao__image">
+        <img src="https://res.cloudinary.com/ap-creative/image/upload/v1620500325/anime/sao/sao_asuna_aincrad_p9ajyj.jpg" alt="Image of The World of the Swords">
+      </picture>
+      <div class="sao__text">
+        <h1 v-if="tablet" class="sao__heading sao__heading--white">The World of Swords</h1>
+        <p class="sao__para sao__para--white">In the year 2022, a next generation game device known as “NerveGear” has been developed, making Full Dives into a virtual dimension possible. “NerveGear” is the world’s first true VRMMORPG device.</p>
+        <p class="sao__para sao__para--white">"Sword Art Online (SAO)" has generated worldwide buzz, and on its official launch day, 10’000 players immerse themselves in the virtual world, only to find out that none of them can escape.</p>
+      </div>
     </section>
-    <SaoContent v-for="block in blocks" :key="block.id" :block="block" :class="'sao__content sao__content__' + block.id"/>
+    <SaoContent v-for="block in blocks" :key="block.id" :block="block" :tablet="tablet" :mobile="mobile" :class="'sao__content sao__content__' + block.id"/>
     <div @click="changeCharacter()">Click Me</div>
-    <section class="sao__characters">
-      <h2 class="sao__heading sao__name">{{ currentCharacter.name }}</h2>
+    <section class="sao__characters sao__content">
+      <h2 class="sao__heading sao__name" v-if="mobile">{{ currentCharacter.name }}</h2>
       <img class="sao__image sao__image--character" :src="currentCharacter.image" :alt="'photo of ' + currentCharacter.name + ' from Sword Art Online'">
-      <p class="sao__para">{{ currentCharacter.para }}</p>
+      <div class="sao__text">
+        <h2 class="sao__heading sao__name" v-if="tablet">{{ currentCharacter.name }}</h2>
+        <p class="sao__para">{{ currentCharacter.para }}</p>
+      </div>
     </section>
   </section>
 </template>
@@ -28,6 +33,9 @@ import axios from 'axios'
 export default {
   data() {
     return {
+      mobile: null,
+      tablet: null,
+
       blocks: [],
       characters: [],
 
@@ -56,6 +64,25 @@ export default {
 
   },
 
+  mounted() {
+
+    if (window.innerWidth <= 767) {
+        this.mobile = true
+        this.tablet = false
+      } else if (window.innerWidth >= 768) {
+        this.tablet = true
+        this.mobile = false
+
+        window.addEventListener("scroll", function() {
+        document.querySelector(".sao__hero").style.opacity = 1 - +window.pageYOffset/4550+ ''
+        })
+      }
+
+    this.$nextTick(function() {
+      window.addEventListener('resize', this.getWindowWidth);
+  })
+  },
+
   methods: {
     changeCharacter() {
       this.currentId += 1
@@ -66,7 +93,24 @@ export default {
       } else {
         this.currentCharacter = this.characters.data[this.currentId]
       }
+    },
+
+    test() {
+      if(this.mobile = null) {
+        console.log("test")
+      }
+    },
+
+    getWindowWidth() {
+      if (window.innerWidth <= 767) {
+        this.mobile = true
+        this.tablet = false
+      } else if (window.innerWidth >= 768) {
+        this.tablet = true
+        this.mobile = false
+      }
     }
+  
   }
 }
 </script>

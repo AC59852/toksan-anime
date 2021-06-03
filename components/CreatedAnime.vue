eea7c33d89cf393a226e16c7edbd679a4c7e09c9
Austin Caron <austin.caron1@gmail.com>
2021-06-03 17:09:02 -0400

    fixing link problem

--- components/CreatedAnime.vue
+++ components/CreatedAnime.vue
@@ -1,5 +1,5 @@
 <template>
-  <nuxt-link :style="{backgroundImage: 'url(' + anime.bck + ')'}" :to="anime.name">
+  <nuxt-link :style="{backgroundImage: 'url(' + anime.bck + ')'}" :to="'/' + anime.name">
       <img class="popAnime__logo" :src="anime.logo" :alt="'Logo for ' + anime.name">
   </nuxt-link>
 </template>
